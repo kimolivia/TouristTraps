2ca25a6cfc0b34cd842333d003af45532b614e51
meganzhao <zhaom@carleton.edu>
2017-11-30 15:09:43 +0100

    added permission and api key to manifest and requestpermission to mainActivity

--- app/src/main/java/hu/ait/android/touristinfo/MainActivity.java
+++ app/src/main/java/hu/ait/android/touristinfo/MainActivity.java
@@ -1,7 +1,13 @@
 package hu.ait.android.touristinfo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestNeededPermission();
     }
+
+    private void requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Toast...
+            }
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    101);
+        } else {
+            // start our job
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 101) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+
+                // start our job
+
+            } else {
+                Toast.makeText(this, "Permission not granted :(", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
